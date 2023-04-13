9519ce85c59368909f3fa80ccf4f5f552c427e15
Pudding <122201777+PuddingNote@users.noreply.github.com>
2023-04-14 01:47:20 +0900

    feat: 플레이어, 카메라 추가 및 생성
    1. New Imput System 추가
    2. Player 오브젝트 추가 (움직임, 애니메이션)
    3. 카메라가 Player 지정

---
+++ Stardew Valley Study/Assets/5. Scripts/PlayerMove.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerMove : MonoBehaviour
+{
+    public Vector2 inputVec;
+    public float moveSpeed;
+
+    Rigidbody2D rigidBody;
+    SpriteRenderer spriteRenderer;
+    Animator anim;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec * moveSpeed * Time.fixedDeltaTime;
+        rigidBody.MovePosition(rigidBody.position + nextVec);
+    }
+
+    void LateUpdate()
+    {
+        //if(inputVec.x != 0)
+        //{
+        //    spriteRenderer.flipX = inputVec.x < 0;
+        //}
+
+        if (inputVec.x != 0 || inputVec.y != 0)
+            anim.SetBool("isMove", true);
+        else
+            anim.SetBool("isMove", false);
+
+        anim.SetFloat("inputX", inputVec.x);
+        anim.SetFloat("inputY", inputVec.y);
+        
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+}
